95108aa7e3f736777f8cb08f7aa20d91766720f2
jk <email@jkrause.io>
2024-06-25 16:17:36 +0200

    social

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,21 @@
-import { defineCollection } from 'astro:content'
-import { projects } from '../types'
+import { defineCollection } from "astro:content";
+import { projects, socials } from "../types";
 
 // Defining Collections to provide all data anybody can update easily
 // Note: Data in YAML needs type 'data', data in Markdown needs type 'content'
 // https://docs.astro.build/en/guides/content-collections/#defining-collections
 
 const projectsCollection = defineCollection({
-  type: 'data',
-  schema: projects
-})
+  type: "data",
+  schema: projects,
+});
+
+const socialCollection = defineCollection({
+  type: "data",
+  schema: socials,
+});
 
 export const collections = {
   projects: projectsCollection,
-}
+  social: socialCollection,
+};
