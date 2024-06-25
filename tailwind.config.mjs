a8b0b8440e2a1a8567f028eaf15ec15041221d15
jk <email@jkrause.io>
2024-06-25 14:30:35 +0200

    ExternaLinkBtn on top on img

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {colors: {
-      // Inspired by https://colorhunt.co/palette/2b39643482aa6db3b5f9cc7b
-      meetup: '#F7931A', // bitcoin
-      tech: '#3482AA', 
-      misc: '#6DB3B5', 
+      btc: '#F7931A', 
     },},
 	},
 	plugins: [],
