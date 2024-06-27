eb72a41ebde97b56634deee80f5fe7c60392d898
jk <email@jkrause.io>
2024-06-27 18:23:16 +0200

    dark/light theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {colors: {
-      btc: '#F7931A', 
-    },},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        btc: "#F7931A",
+      },
+    },
+  },
+  plugins: [],
+};
