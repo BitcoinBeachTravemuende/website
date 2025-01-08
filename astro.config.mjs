7aa451aa1e465e28efb9359375690066e588b800
jk <47693+sectore@users.noreply.github.com>
2025-01-08 13:00:02 +0100

    start `base` with a forward slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://bitcoinbeachtravemuende.github.io',
-  base: 'website',
+  site: "https://bitcoinbeachtravemuende.github.io",
+  base: "/website",
   i18n: {
     defaultLocale: "de",
     locales: ["de", "en"],
