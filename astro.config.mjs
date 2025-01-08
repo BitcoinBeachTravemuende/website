20d1691c73a80cb73a26762003b1ab42e2aaf124
jk <47693+sectore@users.noreply.github.com>
2025-01-08 12:18:05 +0100

    add site|base to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://bitcoinbeachtravemuende.github.io',
+  base: 'website',
   i18n: {
     defaultLocale: "de",
     locales: ["de", "en"],
